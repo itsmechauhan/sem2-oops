83d53be1aa6462c743d4fbe554a7168adbe64b2e
Himanshu Chauhan <103061808+itsmechauhan@users.noreply.github.com>
2024-03-19 03:00:26 +0530

    Update 12.DA-TA.cpp

--- 12.DA-TA.cpp
+++ 12.DA-TA.cpp
@@ -1,29 +1,47 @@
 // Write a C++ program to read the data of N employee and compute the net salary of each 
 // employee (DA=52% of basic and IT=30% of gross salary).
-#include<iostream>
+#include <iostream>
 using namespace std;
-struct employee{
-    char name[20];
+
+struct Employee {
+    int empId;
+    float bs;
+    float grossSalary;
+    float netSalary;
 };
-int net_salary(int bs)
-{
-    int da,it;
-    da=bs*0.52;
-    it=bs*0.30;
-    return(bs+it+da);
-}
+
 int main() {
-    struct employee x[100];
-    int n,bs,total;
-    cout<<"enter number of employees : ";
-    cin>>n;
-    for(int i=0;i<n;i++)
-    {
-        cout<<"\nenter name of employee : ";
-        cin>>x[i].name;
-        cout<<"enter basic salary of employee : ";
-        cin>>bs;
-        total=net_salary(bs);
-        cout<<"gross salary of employee is "<<total;
+    int N;
+    cout << "Enter the number of employees: ";
+    cin >> N;
+
+    Employee emp[N];
+
+    // Input employee data
+    for (int i = 0; i < N; ++i) {
+        cout << "\nEnter details for employee " << i + 1 << ":\n";
+        cout << "Employee ID: ";
+        cin >> emp[i].empId;
+        cout << "Basic Salary: ";
+        cin >> emp[i].bs;
+        // Calculate gross salary
+        emp[i].grossSalary = emp[i].bs + (0.52 * emp[i].bs);
     }
+
+    // Calculate net salary
+    for (int i = 0; i < N; ++i) {
+        float it = 0.3 * emp[i].grossSalary;
+        emp[i].netSalary = emp[i].grossSalary - it;
+    }
+
+    // Display employee data
+    cout << "\nEmployee details with net salary:\n";
+    for (int i = 0; i < N; ++i) {
+        cout << "\nEmployee ID: " << emp[i].empId << endl;
+        cout << "Basic Salary: " << emp[i].bs << endl;
+        cout << "Gross Salary: " << emp[i].grossSalary << endl;
+        cout << "Net Salary: " << emp[i].netSalary << endl;
+    }
+
+    return 0;
 }
