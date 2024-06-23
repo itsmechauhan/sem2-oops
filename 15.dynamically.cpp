bf3c56cc760c7d2c521f9425c154ad42186d1cb7
Himanshu Chauhan <103061808+itsmechauhan@users.noreply.github.com>
2024-06-23 21:42:23 +0530

    Update 15.dynamically.cpp

--- 15.dynamically.cpp
+++ 15.dynamically.cpp
@@ -6,27 +6,19 @@ int main() {
     int *ptr = new int;
     
     // Check if memory allocation is successful
-    if (ptr == nullptr) {
+    if (ptr == NULL) {
         cout << "Memory allocation failed!";
         return 1;
     }
     
     // Store a value in the dynamically allocated memory
-    *ptr = 10;
+    cout<<"Enter a value: ";
+    cin>>*ptr;
     
     // Display the value
     cout << "Dynamically allocated value: " << *ptr << endl;
     
     // Deallocate the dynamically allocated memory
     delete ptr;
-    
-    // Check if deallocation is successful
-    if (ptr != nullptr) {
-        cout << "Memory deallocation failed!";
-        return 1;
-    }
-    
-    cout << "Memory deallocated successfully!";
-    
-    return 0;
+
 }
