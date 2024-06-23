46eda89ed98675c3f3c94c2b139977f3669bd6a1
Himanshu Chauhan <103061808+itsmechauhan@users.noreply.github.com>
2024-06-23 21:43:22 +0530

    Update 13.global-local.cpp

--- 13.global-local.cpp
+++ 13.global-local.cpp
@@ -1,9 +1,10 @@
  // Write a C++ program to use scope resolution operator. Display the various values of the same 
 // variables declared at different scope levels. 
 #include<iostream>
+using namespace std;
 int a=20;
 int main() {
     int a=10;
-    std::cout<<"\nvalue of locally declared variable a is  :"<<a;
-    std::cout<<"\nvalue of globally declared variable a is :"<<::a;
+    cout<<"\nvalue of locally declared variable a is  :"<<a;
+    cout<<"\nvalue of globally declared variable a is :"<<::a;
 }
